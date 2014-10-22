6efa40e435180782f460277de978b51e335aa15f
marcoradavelli <marcora@stud.ntnu.no>
2014-10-22 19:26:59 +0200

    Added IP banning, don't know if it works yet

--- src/webapp/controllers/LoginController.php
+++ src/webapp/controllers/LoginController.php
@@ -24,6 +24,14 @@ class LoginController extends Controller
 
     function login()
     {
+        $BFBresponse = BruteForceBlock::getLoginStatus();
+        if ($BFBresponse['status']=='delay') {
+            //time delay required before next login
+            $this->app->flash('error', "Wait $BFBresponse[message] seconds before login.");
+            $this->app->redirect('/');
+            die();
+        }
+
         $request = $this->app->request;
         $user = $request->post('user');
         $pass = $request->post('pass');
@@ -41,6 +49,10 @@ class LoginController extends Controller
             $this->app->flash('info', "You are now successfully logged in as $user.");
             $this->app->redirect('/');
         } else {
+            $userFound = User::findByUser($user);
+            $user_id = $userFound == null ? 0 : $userFound->id;
+            $ip_addressget = get_client_ip();
+            BruteForceBlock::addFailedLoginAttempt($user_id, $ip_addressget);
             $this->app->flashNow('error', 'Incorrect user/pass combination.');
             $this->render('login.twig', []);
         }
